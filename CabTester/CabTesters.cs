7882d8a6e7f2a343b5d264e88e989c278a4afb31
Avenger2627 <imraninfo97@gmail.com>
2020-12-23 19:11:28 +0530

    [Imran] Add: Calculate Fare For Multuple Ride Retrun Summary

--- CabTester/CabTesters.cs
+++ CabTester/CabTesters.cs
@@ -8,9 +8,8 @@ namespace CabInvoiceGenratorTest
         InvoiceGenerator invoiceGenerator = null;
 
         /// <summary>
-        /// Test case for UC 1 Given the distance and time when invoice generator then should return total fare.
+        /// Test case for UC 1 Given the distance and time when invoice generator return total fare.
         /// </summary>
-        
         [Test]
         public void GivenDistanceAndTime_ReturnTotalFare()
         {
@@ -23,16 +22,28 @@ namespace CabInvoiceGenratorTest
         }
 
         /// <summary>
-        /// TestCase for UC 2 Givens the multiple rides  return invoice summary.
+        /// Test case for UC 2 Givens the multiple rides  return invoice summary.
         /// </summary>
-        
         [Test]
-        public void GivenMultipleRides__ShouldReturnInvoiceSummary()
+        public void GivenMultipleRides_WhenInvoiceGenerator_thenShouldReturnInvoiceSummary()
         {
             invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
             Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
             InvoiceSummary invoiceSummary = invoiceGenerator.CalculateFare(rides);
             InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0);
+            Assert.AreEqual(expectedSummary, invoiceSummary);
+        }
+
+        /// <summary>
+        /// Test case 3 Givens the multiple rides  return  invoice summary.
+        /// </summary>
+        [Test]
+        public void GivenMultipleRides_WhenInvoiceGenerator_thenShouldReturnFollowingInvoiceSummary()
+        {
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+            InvoiceSummary invoiceSummary = invoiceGenerator.CalculateAvrageFare(rides);
+            InvoiceSummary expectedSummary = new InvoiceSummary(2, 30.0, 15);
             Assert.AreEqual(expectedSummary, invoiceSummary);
         }
     }
